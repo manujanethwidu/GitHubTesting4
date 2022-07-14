2f71f86bca19bcd111f6f9da5c903cc1bf6d2087
manujanethwidu <manujanethwidu@gmail.com>
2022-07-14 09:42:13 +0530

    3rd Commit

--- app/src/main/java/com/mnm/githubtesting4/MainActivity.kt
+++ app/src/main/java/com/mnm/githubtesting4/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("First commit")
         println("Second Commit")
+        println("3rd commit")
+
     }
 }
