c585c98fd6d649952eeeee16c6c5d3e54315e1ef
manujanethwidu <manujanethwidu@gmail.com>
2022-07-14 09:37:25 +0530

    Second Commit

--- app/src/main/java/com/mnm/githubtesting4/MainActivity.kt
+++ app/src/main/java/com/mnm/githubtesting4/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("First commit")
+        println("Second Commit")
     }
 }
